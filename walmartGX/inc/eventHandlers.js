40a5886cdb854e74bde70ddb66cc0b938f38c8a4
John Vogen <john.vogen@verizon.net>
2017-04-14 19:05:35 -0400

    step 2 of multiple player integration - not there yet bu advancing. Now we need to add multiple videos and test/program functionality as needed - back button too.

--- walmartGX/inc/eventHandlers.js
+++ walmartGX/inc/eventHandlers.js
@@ -13,9 +13,16 @@ function question6_element2() {
 }
 
 // This event is specified in the config file
-function showSlideInfo() {
-    $('#slideInfo').toggle("slide", { direction: "right" }, 400);
-    myPlayer.pause();
+function showSlideInfo(slide) {
+
+    if (slide == 1) {
+        $('#slideInfo').toggle("slide", { direction: "right" }, 400);
+        myPlayer.pause();
+    } else if (slide == 2) {
+        $('#slideInfo2').toggle("slide", { direction: "right" }, 400);
+        myPlayer2.pause();
+
+    }
 }
 
 //This event is specified in the slideInfo.html file
@@ -23,6 +30,14 @@ function dismissSlideInfo() {
     $('#slideInfo').toggle("slide", { direction: "right" }, 400);
     myPlayer.play();
 }
-    
+function dismissSlideInfo2() {
+    $('#slideInfo2').toggle("slide", { direction: "right" }, 400);
+    myPlayer2.play();
+}
+
+function takeAction() {
+    alert("Taking Action");
+}
+
 
 //end custom events section
--- walmartGX/js/app.js
+++ walmartGX/js/app.js
@@ -3,11 +3,12 @@ var myPlayer = videojs("myPlayerID");
 var myPlayer2 = videojs("myPlayerID2");
 var config = {};
 var configs = [];
+var waitSequenceShowing = false;
 
 var repositoryId = "a657fd8fced304aeb5cc";
 var branchId = "e107b525b507b9d33a23";
 var nodeId = "1f89a17e1845bc71d945"; // Walmart
-
+$("#myPlayerID2").attr("data-video-id", "5397231147001");
 var config1 = {
     "clientKey": "ac8a94d2-05d0-4d03-919d-52408a8f9c06",
     "clientSecret": "wn7qJcPQVucXl3IrtJpPfmX7sfJORbAdA9DL/0wEjvcYU5g3Qzu4lKSTZKjHrI+iMOqUSC5aPoSkazluD+i95vuLVSjBtzYm3y2296ESC6Y=",
@@ -27,35 +28,31 @@ Gitana.connect(config1, function (err) {
             this.readNode(nodeId).then(function () {
                 config = JSON.parse(JSON.stringify(this));
                 loadNewVideo(config.startVideoName, false);
+                loadWaitSequence(config.videos[1].brightcoveId, false);//the wait sequence of the starting video
             });
         });
     });
 });
 
 videojs("myPlayerID").ready(function () {
-
+    //alert("player 1 ready");
 
 
 
     myPlayer.on("ended", function () {
-
-        if (config.videos[config.currentVideoIndex].endBehavior != undefined) {
-
-            
-
+        if (config.videos[config.currentVideoIndex].endBehavior != undefined) {      
             $("#myPlayerIDContainer").css("display", "none");
             $("#myPlayerID2Container").css("display", "block");
             myPlayer2.play();
+            myPlayer.pause();
             //loadNewVideo(config.videos[config.currentVideoIndex].endBehavior, false);
-
-
         } else {
             myPlayer.pause();//pause the player if no endbehavior is specified
         }
     })
 
     myPlayer.on("loadedmetadata", function () {
-
+        //alert("player1 metadata loaded");
         console.log("Text Track to load: " + config.videos[config.currentVideoIndex].tracks);
         if (config.videos[config.currentVideoIndex].tracks != undefined) {
             myPlayer.addRemoteTextTrack({
@@ -63,7 +60,6 @@ videojs("myPlayerID").ready(function () {
                 src: "vtt/" + config.videos[config.currentVideoIndex].tracks + ".vtt"
             }, false);
         } else {
-
         }
         var trackIndex = myPlayer.textTracks().length - 1;
         var tt = myPlayer.textTracks()[trackIndex];
@@ -83,23 +79,55 @@ videojs("myPlayerID").ready(function () {
                 console.log("Cue point duration over");
             }
         }
-
-        
+        //alert("player 1 starting to play");
         myPlayer.play();
         myPlayer2.pause();
         $("#slideInfo").load("inc/" + config.startVideoName + ".html");
-
         makeVideoOverlayWait("intro_wait");
-
-
     });
 });
 
 
 
+videojs("myPlayerID2").ready(function () {
+    myPlayer2.on("ended", function () {
+        myPlayer2.currentTime(0);
+    })
 
-
-
+    myPlayer2.on("loadedmetadata", function () {
+        //alert("Player 2 metadata loaded");
+        console.log("Text Track to load: " + config.videos[config.currentVideoIndex].tracks);
+        //if (config.videos[config.currentVideoIndex].tracks != undefined) {
+        //    myPlayer.addRemoteTextTrack({
+        //        kind: 'metadata',
+        //        src: "vtt/" + config.videos[config.currentVideoIndex].tracks + ".vtt"
+        //    }, false);
+        //} else {
+        //}
+        //var trackIndex = myPlayer.textTracks().length - 1;
+        //var tt = myPlayer.textTracks()[trackIndex];
+        //tt.oncuechange = function () {
+        //    if (tt.activeCues[0] !== undefined) {
+        //        console.log("Cue point begins");
+        //        var dynamicHTML = "id: " + tt.activeCues[0].id + ", ";
+        //        dynamicHTML += "text: " + tt.activeCues[0].text + ", ";
+        //        dynamicHTML += "startTime: " + tt.activeCues[0].startTime + ",  ";
+        //        dynamicHTML += "endTime: " + tt.activeCues[0].endTime;
+        //        console.log(dynamicHTML);
+        //        jsonData = JSON.parse(tt.activeCues[0].text);
+        //        if (jsonData.description == "results") {
+        //            showResults();
+        //        }
+        //    } else {
+        //        console.log("Cue point duration over");
+        //    }
+        //}
+        //myPlayer.play();
+        myPlayer2.play();
+        waitSequenceShowing = true;
+        $("#slideInfo2").load("inc/" + "intro_wait" + ".html");
+    });
+});
 
 
 
@@ -133,7 +161,13 @@ function skip() {
     if (config.videos[config.currentVideoIndex].skipIntro && (myPlayer.currentTime() < config.videos[config.currentVideoIndex].skipIntro)) {
         myPlayer.currentTime(config.videos[config.currentVideoIndex].skipIntro)
     } else {
-        loadNewVideo(config.videos[config.currentVideoIndex].endBehavior, true);
+        if (waitSequenceShowing) {//only jump if this is not the wait sequence video showing in player 2
+            //alert("jumping to end");
+            loadNewVideo(config.videos[config.currentVideoIndex].endBehavior, true);
+        } else {
+            //alert("NMot jumping to end");
+        }
+
     }
 }
 
@@ -141,6 +175,14 @@ function back() {
     config = JSON.parse(configs.pop());
 
     if (config.videos[config.currentVideoIndex].name == config.startVideoName) { //First video starting - reset voteBucket values to 0
+
+
+
+
+        $("#myPlayerIDContainer").css("display", "block");
+        $("#myPlayerID2Container").css("display", "none");
+        myPlayer2.pause();
+        myPlayer.play();
         location.reload;
     }
     try {
@@ -165,7 +207,6 @@ function loadNewVideo(videoId, saveThis) {
         }
         configs.push(JSON.stringify(config));
     }
-    debugger;
     //The following do/while loop translates the name passed in to the brightcove id pertaining to that video name. This allows one video to be used for many questions/answers. Name based rather than brightcoveid based
     var iterator = 0
     do {
@@ -192,7 +233,6 @@ function loadNewVideo(videoId, saveThis) {
 }
 
 function makeVideoOverlay(videoId) {
-    debugger;
     //set global json properties
     var videoOverlayObject = {};
     videoOverlayObject.overlay = {};
@@ -217,7 +257,7 @@ function makeVideoOverlay(videoId) {
             //now add the menubar overlay to every page
             var navBar = {};
             navBar.align = "bottom-left";
-            navBar.content = "<span onclick='" + config.homeVideoEvent + "()'><i class='fa fa-2x " + config.homeVideoIcon + " text-primary sr-icons'></i></span><span onclick='" + config.skipVideoEvent + "()'><i class='fa fa-2x " + config.skipVideoIcon + " text-primary sr-icons'></i></span><span onclick='" + config.backVideoEvent + "()'><i class='fa fa-2x " + config.backVideoIcon + " text-primary sr-icons'></i></span>" + "<span onclick='" + config.infoVideoEvent + "()'><i class='fa fa-2x " + config.infoVideoIcon + " text-primary sr-icons'></i></span>";
+            navBar.content = "<span onclick='" + config.homeVideoEvent + "()'><i class='fa fa-2x " + config.homeVideoIcon + " text-primary sr-icons'></i></span><span onclick='" + config.skipVideoEvent + "()'><i class='fa fa-2x " + config.skipVideoIcon + " text-primary sr-icons'></i></span><span onclick='" + config.backVideoEvent + "()'><i class='fa fa-2x " + config.backVideoIcon + " text-primary sr-icons'></i></span>" + "<span onclick='" + config.infoVideoEvent + "(1)'><i class='fa fa-2x " + config.infoVideoIcon + " text-primary sr-icons'></i></span>";
             navBar.start = 0;
             navBar.end = config.videos[config.currentVideoIndex].duration;
             videoOverlayObject.overlay.overlays.push(navBar);
@@ -227,8 +267,25 @@ function makeVideoOverlay(videoId) {
     }
 }
 
+function loadWaitSequence(videoId) {
+    //alert("loading seconf video")
+    myPlayer2.catalog.getVideo(videoId, function (error, video) {
+        //deal with error
+        myPlayer2.catalog.load(video);
 
-function makeVideoOverlayWait(videoId) {
+        makeVideoOverlayWait("intro_wait");
+        //if (name == config.startVideoName) { //First video starting - reset voteBucket values to 0
+        //    for (var i = 0; i < config.recommendations.length; i++) {
+        //        config.recommendations[i].voteBucket = 0;
+        //    }
+        //}
+        myPlayer2.play(); //Call the play method within the loadedmetadata event rather than here.
+    });
+}
+
+
+
+function makeVideoOverlayWait(videoName) {
     //set global json properties
     var videoOverlayObject = {};
     videoOverlayObject.overlay = {};
@@ -237,7 +294,7 @@ function makeVideoOverlayWait(videoId) {
 
     //now create the overlay properties
     for (var i = 0; i < config.videos.length; i++) { //find the current video object in the config object
-        if (config.videos[i].name == videoId) {
+        if (config.videos[i].name == videoName) {
             //config.currentVideoIndex = i;
             for (var j = 0; j < config.videos[i].onscreenElements.length; j++) { // cycle through the onscreenElements array and build an overly for each
                 videoOverlayObject.overlay.overlays[j] = {};
@@ -253,7 +310,7 @@ function makeVideoOverlayWait(videoId) {
             //now add the menubar overlay to every page
             var navBar = {};
             navBar.align = "bottom-left";
-            navBar.content = "<span onclick='" + config.homeVideoEvent + "()'><i class='fa fa-2x " + config.homeVideoIcon + " text-primary sr-icons'></i></span><span onclick='" + config.skipVideoEvent + "()'><i class='fa fa-2x " + config.skipVideoIcon + " text-primary sr-icons'></i></span><span onclick='" + config.backVideoEvent + "()'><i class='fa fa-2x " + config.backVideoIcon + " text-primary sr-icons'></i></span>" + "<span onclick='" + config.infoVideoEvent + "()'><i class='fa fa-2x " + config.infoVideoIcon + " text-primary sr-icons'></i></span>";
+            navBar.content = "<span onclick='" + config.homeVideoEvent + "()'><i class='fa fa-2x " + config.homeVideoIcon + " text-primary sr-icons'></i></span><span onclick='" + config.skipVideoEvent + "()'><i class='fa fa-2x " + config.skipVideoIcon + " text-primary sr-icons'></i></span><span onclick='" + config.backVideoEvent + "()'><i class='fa fa-2x " + config.backVideoIcon + " text-primary sr-icons'></i></span>" + "<span onclick='" + config.infoVideoEvent + "(2)'><i class='fa fa-2x " + config.infoVideoIcon + " text-primary sr-icons'></i></span>";
             navBar.start = 0;
             navBar.end = config.videos[config.currentVideoIndex + 1].duration;
             videoOverlayObject.overlay.overlays.push(navBar);
