5753d966a4e12c5625ee26540cfb8872f6dce199
John Vogen <john.vogen@verizon.net>
2017-04-18 12:37:18 -0400

    Remove extra event listeners

--- walmartGX/inc/eventHandlers.js
+++ walmartGX/inc/eventHandlers.js
@@ -1,16 +1,5 @@
 ﻿//custom events section
-function question6_element1() {
-    //loadNewVideo(config.videos[config.currentVideoIndex].onscreenElements[1].defaultAction.jumpToName, true);
-    //assignWeights(config.videos[config.currentVideoIndex].onscreenElements[1].defaultAction.weightings);
 
-    loadNewVideo("question8", true);
-    assignWeights([10, 1, 0, 0, 0, 0, 0]);
-
-}
-function question6_element2() {
-    loadNewVideo(config.videos[config.currentVideoIndex].onscreenElements[2].defaultAction.jumpToName, true);
-    assignWeights(config.videos[config.currentVideoIndex].onscreenElements[2].defaultAction.weightings);
-}
 
 // This event is specified in the config file
 function showSlideInfo(slide) {
